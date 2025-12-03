33e7db50f73413841dd4e8d66c3f3110582b3938
sarah-larkin <larkinasarah@gmail.com>
2025-12-03 15:21:00 +0000

    split so can run separately

--- day3.sql
+++ day3.sql
@@ -11,8 +11,41 @@
 -- SELECT * FROM authors;
 -- use \q to escape the terminal if needed
 
+--answers: 
+
 UPDATE books
 SET author_id = 3
-WHERE book_title LIKE %Hitchhiker%;
+WHERE book_title LIKE '%Hitchhiker%';
 
-SELECT * FROM books;
+--use subquery: 
+
+UPDATE books
+SET author_id = (SELECT author_id FROM authors WHERE author_name = 'Antoine de Saint-Exupéry')
+WHERE book_title = 'The Little Prince';
+
+UPDATE books
+SET author_id = (SELECT author_id FROM authors WHERE author_name = 'Beatrix Potter')
+WHERE book_title = 'The Tale of Peter Rabbit';
+
+-- UPDATE books
+-- SET author_id = (SELECT author_id FROM authors WHERE author_name = 'Jane Austen')
+-- WHERE book_title = 'Emma';
+
+UPDATE books
+SET author_id = (SELECT author_id FROM authors WHERE author_name = 'George Orwell')
+WHERE book_title LIKE 'Nineteen Eighty-Four%';
+
+UPDATE books
+SET author_id = (SELECT author_id FROM authors WHERE author_name = 'Margaret Atwood')
+WHERE book_title = 'The Handmaid''s Tale';  -- double '' for apostrophe 
+
+UPDATE books
+SET author_id = (SELECT author_id FROM authors WHERE author_name = 'H. G. Wells')
+WHERE book_title = 'The War of the Worlds';
+
+UPDATE books
+SET author_id = (SELECT author_id FROM authors WHERE author_name = 'Jane Austen')
+WHERE book_title IN ('Pride and Prejudice', 'Emma');
+
+SELECT * FROM books
+ORDER BY book_id;
