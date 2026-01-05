6f66809fbf638b85d5642d01e734f20f4b66a836
sarah-larkin <larkinasarah@gmail.com>
2026-01-05 10:16:30 +0000

    setup ready

--- db2/recipe.sql
+++ db2/recipe.sql
@@ -1,2 +1,25 @@
 \c recipebook
 
+-- Find all the recipes that use all of a certain set of ingredients.
+
+
+
+-- Find all the recipes which use one or more of a certain set of ingredients.
+
+
+
+-- more advanced filtering: 
+-- Find all the pizzas that include garlic or tomatoes but not mushrooms.
+
+
+
+-- add new table "methods" (without dropping the db)
+-- adjust the current tables (preserve original data)
+
+
+
+-- queries: 
+-- Find all of the methods used in starters.
+-- Find all of the ingredients that use a specific method.
+-- Find all of the ingredients that use more than 2 methods.
+-- Find all of the ingredients for a specific recipe and the relevant methods used for those ingredients
